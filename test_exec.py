544091182fa9c2a4d372a775e66e25bf60ffd2cc
Peter Wickersham <pwickersham@gmail.com>
2026-04-10 19:21:49 -0700

    summary: "Extended pull_base_branch() in workspace.py to snapshot the gi

---
+++ test_exec.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+if len(sys.argv) > 1 and sys.argv[1] == "reexec":
+    print("Re-execed successfully!")
+    sys.exit(0)
+
+print(f"sys.executable: {sys.executable}")
+print(f"sys.argv: {sys.argv}")
+os.execv(sys.executable, sys.argv + ["reexec"])
